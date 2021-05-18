9a108722a964cfba90c2089160edd55032834f2d
JORGE <jcgarciamed@gmail.com>
2021-05-18 16:31:13 -0500

    Version 0

---
+++ src/main/java/com/greymatter/gmrecipeapp/GmRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greymatter/gmrecipeapp/GmRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
