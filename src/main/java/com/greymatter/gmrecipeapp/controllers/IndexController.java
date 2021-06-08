7110d3ce6682d359dbf9e0b72264e532f27a50d5
JORGE <jcgarciamed@gmail.com>
2021-06-08 00:04:47 -0500

    Added Project Lombok.

--- src/main/java/com/greymatter/gmrecipeapp/controllers/IndexController.java
+++ src/main/java/com/greymatter/gmrecipeapp/controllers/IndexController.java
@@ -1,17 +1,13 @@
 package com.greymatter.gmrecipeapp.controllers;
 
-import com.greymatter.gmrecipeapp.domain.Category;
-import com.greymatter.gmrecipeapp.domain.UnitOfMeasure;
-import com.greymatter.gmrecipeapp.repositories.CategoryRepository;
-import com.greymatter.gmrecipeapp.repositories.UnitOfMeasureRepository;
+
 import com.greymatter.gmrecipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.SQLOutput;
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"/", "", "index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
