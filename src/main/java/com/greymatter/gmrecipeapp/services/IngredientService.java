46e9aad170637e38bf36639acc5f487c26a4e5a8
JORGE <jcgarciamed@gmail.com>
2021-07-21 14:27:11 -0500

    delete an ingredient.

--- src/main/java/com/greymatter/gmrecipeapp/services/IngredientService.java
+++ src/main/java/com/greymatter/gmrecipeapp/services/IngredientService.java
@@ -6,4 +6,6 @@ import com.greymatter.gmrecipeapp.commands.IngredientCommand;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
+
 }
