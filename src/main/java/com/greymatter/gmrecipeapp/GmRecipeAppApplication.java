0f1b73bb288932786ffe454650d40cf3c362ce7b
JORGE <jcgarciamed@gmail.com>
2021-08-08 04:04:55 -0500

    I18N

--- src/main/java/com/greymatter/gmrecipeapp/GmRecipeAppApplication.java
+++ src/main/java/com/greymatter/gmrecipeapp/GmRecipeAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GmRecipeAppApplication {
-    //THIS IS A TEST
+    //THIS IS A TEST 2 223
     public static void main(String[] args) {
         SpringApplication.run(GmRecipeAppApplication.class, args);
     }
